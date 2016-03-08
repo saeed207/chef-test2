53a8283ff14e3734fe07bec9c998469a72762156
Saeed <saeed207@gmail.com>
2016-03-08 03:35:15 +0200

    adding service stop

--- saeed/recipes/mbc3.rb
+++ saeed/recipes/mbc3.rb
@@ -2,6 +2,17 @@
 
 include_recipe 'deploy'
 
+service "apache2" do
+  action [ :stop ]
+end
+
+bash "stop_tomcat" do
+  user "root"
+  code do
+    for (( ; ; ));do java=''; java=`ps -ef | grep java | grep -vi grep | awk '{print $2}'`; if [ $java != "" ]; then pkill -15 java; else break; fi; done
+  end
+end
+
 node[:deploy].each do |application, deploy|
   opsworks_deploy_dir do
     user deploy[:user]
@@ -17,7 +28,11 @@ node[:deploy].each do |application, deploy|
 execute 'deployment_finalization' do
   command 'cp -r /usr/local/apache-tomcat/latest/opsworks-deployment/current/* /usr/local/apache-tomcat/latest/webapps/'
   cwd "/usr/local/apache-tomcat/latest/webapps"
+  user "tomcat"
+  group "tomcat"
  end
+
+
 
 execute 'deployment_cleanup' do
   command 'rm -rf config log tmp public'
